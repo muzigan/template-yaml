8a12075523d9224916a46b91762acefd1ea3ece0
muzigan <muzigan@foxmail.com>
2019-12-10 14:58:31 +0800

    test

--- scripts/image-mv-google_containers.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#for i in ` docker images |awk  -v OFS=':' '{print $1,$2}'| grep  "registry.aliyuncs.com/google_containers"` ;do
-#	NEWNAME=`echo $i |sed "s@registry.aliyuncs.com/google_containers@registry.cn-hangzhou.aliyuncs.com/wanway-base@g"`
-#	docker tag $i $NEWNAME
-#	docker push $NEWNAME
-#        [ "$?" -ne "0" ] && echo "docker push $NEWNAME" >> ./errorimages
-#done
-
-
